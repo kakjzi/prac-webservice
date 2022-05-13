8cf05543dc939043dd82d9cd1bba43ae63ce564c
kakjzi <anmern3@naver.com>
2022-05-13 17:15:36 +0900

    글 등록 page 추가

--- src/main/java/com/kakjzi/book/springboot/web/IndexController.java
+++ src/main/java/com/kakjzi/book/springboot/web/IndexController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postSave() {
+        return "posts-save";
     }
 }
