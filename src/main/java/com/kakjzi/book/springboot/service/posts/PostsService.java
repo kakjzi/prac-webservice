7033f9669b775f86212ad1675a5c32d9329c58b2
kakjzi <anmern3@naver.com>
2022-05-13 18:28:35 +0900

    조회 기능 추가

--- src/main/java/com/kakjzi/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/kakjzi/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.kakjzi.book.springboot.service.posts;
 
 import com.kakjzi.book.springboot.domain.posts.Posts;
 import com.kakjzi.book.springboot.domain.posts.PostsRepository;
+import com.kakjzi.book.springboot.web.dto.PostsListResponseDto;
 import com.kakjzi.book.springboot.web.dto.PostsResponseDto;
 import com.kakjzi.book.springboot.web.dto.PostsSaveRequestDto;
 import com.kakjzi.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,6 +40,15 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+
 }
 
 /**
